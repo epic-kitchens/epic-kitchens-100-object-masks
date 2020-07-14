1c20ac253421e9ac2c3ea8dab0590effeefe59a7
Will Price <will.price94@gmail.com>
2020-07-14 11:19:57 +0100

    Add dependency on pycocotools

--- setup.py
+++ setup.py
@@ -125,7 +125,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['dataclasses', 'numpy', 'protobuf', 'pillow', 'scikit-image'],
+    install_requires=['dataclasses', 'numpy', 'protobuf', 'pillow', 'scikit-image', 'pycocotools'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
